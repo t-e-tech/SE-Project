e51d95c02efaf7bb4dfd29cb59911ddf342a87b5
PAMMJUNIOR <haruethai.u@ku.th>
2023-04-06 02:42:31 +0700

    update

--- เต้/client frontend/src/screens/PlaceOrderScreen.js
+++ เต้/client frontend/src/screens/PlaceOrderScreen.js
@@ -22,8 +22,8 @@ const PlaceOrderScreen = ({ history }) => {
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
-  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
-  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.shippingPrice = addDecimals(30);
+  cart.taxPrice = addDecimals(Number((0 * cart.itemsPrice).toFixed(2)));
   cart.totalPrice = (
     Number(cart.itemsPrice) +
     Number(cart.shippingPrice) +
